d82a2051e0215a22400719420ac45a6987c323be
Martin Hujer <mhujer@gmail.com>
2012-03-03 22:32:50 +0100

    - formát; generování API dokumentace

---
+++ phpdoc/phpdoc01.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Ukázková třída
+ * 
+ * @author Martin Hujer
+ */
+class MyClass
+{
+    /**
+     * Funkce foo nic nedělá, jen vrátí předaný parametr.
+     * 
+     * @param boolean $param Předaný parametr
+     * @returxn boolean
+     */
+    public function foo($param)
+    {
+        return $param;
+    }
+}
