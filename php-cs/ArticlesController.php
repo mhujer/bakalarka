42690a814ec272b32125adc30f30fd3ab421a129
Martin Hujer <mhujer@gmail.com>
2012-03-17 15:38:16 +0100

    PHPCS - example

---
+++ php-cs/ArticlesController.php
@@ -0,0 +1,8 @@
+<?php
+class ArticlesController extends Zend_Controller_Action{
+    /**
+     * Index action
+     */
+    public function indexAction(){
+             $a = new ArticleModel();    }
+    }
